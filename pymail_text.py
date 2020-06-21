f74e620882078110a940370743eb005f90da10ff
Zulfikar Ahmed <zulfikar.ahmed@gmail.com>
2020-06-21 15:08:47 -0500

    This is working.

---
+++ pymail_text.py
@@ -0,0 +1,50 @@
+from pyspark import SparkConf, SparkContext
+#import pymongo
+#import pymongo_spark
+import smtplib
+from operator import add
+import sys
+
+
+def send_email2( mfrom, mto, msg ):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    #Next, log in to the server
+    server.login("youremailusername", "password")
+    #Send the mail
+    #msg = "
+    #Hello!" # The /n separates the message from the headers
+    server.sendmail(mfrom, mto, msg)
+    return mto+' ok'
+
+def send_email( mfrom, mto, msg):
+    return('sent ' + msg + ' to '+mto)
+
+## Constants
+APP_NAME = " HelloWorld of Big Data"
+##OTHER FUNCTIONS/CLASSES
+mongo_url = 'mongodb://unique:unique@cluster0.3cmqe.mongodb.net/'
+
+def main(sc, db_dot_collection):
+    mfrom = 'myself@whatever.com'
+    filename = "emailsvc.csv"
+    msg = "Hello"
+    textRDD = sc.textFile(filename)
+    words = textRDD.flatMap(lambda x: x.split(' ')).map(lambda to: send_email(mfrom,to,msg))
+    z = words.collect()
+    for y in z:
+        print(y)
+    #wordcount = words.reduceByKey(add).collect()
+    #for wc in wordcount:
+    #    print wc[0],wc[1]
+    #msg = "hello"
+    #pymongo_spark.activate()
+    #rdd = (sc.mongoRDD('{0}{1}'.format(mongo_url,db_dot_collection)).map(lambda doc: send_email(mfrom, doc.get('email'), msg)))
+    #rdd.collect()
+
+
+   # Configure Spark
+conf = SparkConf().setAppName(APP_NAME)
+conf = conf.setMaster("local[*]")
+sc   = SparkContext(conf=conf)
+# Execute Main functionality
+main(sc, 'test.emailsvc')
